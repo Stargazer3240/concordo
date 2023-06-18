b38a9647c830e1d70fd6ebe6259ef54e501b72ef
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-06-18 18:16:29 -0300

    Made a getId() and made string getters return by reference

--- include/users.h
+++ include/users.h
@@ -10,12 +10,14 @@
 
 class User {
  public:
-  User(std::string_view name, std::string_view email, std::string_view password)
+  User(std::string_view name, std::string_view email,  // NOLINT
+       std::string_view password)                      // NOLINT
       : name{name}, email{email}, password{password} {}
 
-  std::string getName() { return name; }
-  std::string getEmail() { return email; }
-  std::string getPassword() { return password; }
+  [[nodiscard]] int getId() const { return id; }
+  std::string& getName() { return name; }
+  std::string& getEmail() { return email; }
+  std::string& getPassword() { return password; }
 
  private:
   int id{};
