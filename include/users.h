a5a34d451636cd635e48a98e067e38b7611a11cb
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-06-21 15:42:36 -0300

    Changed Password and Email classes to struct, and made a Name struct

--- include/users.h
+++ include/users.h
@@ -5,67 +5,40 @@
 #ifndef USERS_H
 #define USERS_H
 
-#include <algorithm>
 #include <string>
 #include <string_view>
 #include <utility>
 
 namespace concordo::user {
 
-using std::string_view, std::string, std::begin, std::find;
+using std::string_view, std::string;
 
-class EmailAddress {
- public:
-  EmailAddress() = default;
-  explicit EmailAddress(string_view a)
-      : username_{parse_username(a)},
-        mail_server_{parse_server(a)},
-        domain_{parse_domain(a)} {}
-
-  static string parse_username(string_view a) {
-    return {begin(a), begin(a) + a.find('@')};
-  };
-  static string parse_server(string_view a) {
-    return {begin(a) + a.find('@') + 1, begin(a) + a.find('.')};
-  };
-  static string parse_domain(string_view a) {
-    return {begin(a) + a.find('.') + 1, end(a)};
-  };
-  [[nodiscard]] string compose_address() const {
-    return username_ + '@' + mail_server_ + '.' + domain_;
-  }
-
- private:
-  string username_;
-  string mail_server_;
-  string domain_;
+struct EmailAddress {
+  string email_address;
 };
 
-class Password {
- public:
-  Password() = default;
-  explicit Password(string_view p) : password_{p} {}
+struct Password {
+  string password;
+};
 
- private:
-  string password_;
+struct Name {
+  string name;
 };
 
 class User {
  public:
   User() = default;
-  User(string_view n, EmailAddress a, Password p)
-      : name_{n}, address_{std::move(a)}, password_{std::move(p)} {}
+  User(Name n, EmailAddress a, Password p)
+      : name_{std::move(n)}, address_{std::move(a)}, password_{std::move(p)} {}
 
   [[nodiscard]] int getId() const { return id_; }
-  [[nodiscard]] string getName() const { return name_; }
-  [[nodiscard]] string getEmail() const {
-    return this->address_.compose_address();
-  }
+  [[nodiscard]] Name getName() const { return name_; }
+  [[nodiscard]] EmailAddress getEmail() const { return address_; }
   [[nodiscard]] Password getPassword() const { return password_; }
 
  private:
   int id_{};
-  string name_;
+  Name name_;
   EmailAddress address_;
   Password password_;
 };
