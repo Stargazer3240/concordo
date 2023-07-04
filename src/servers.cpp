ffd112ea21ae410888ff3f2daea8eb5ce50210af
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-07-04 18:18:55 -0300

    Restarting the project

--- src/servers.cpp
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Fabrício Moura Jácome
-//
-// SPDX-License-Identifier: MIT
-
-#include "servers.h"
-
-namespace server {}  // namespace server
--- src/users.cpp
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Fabrício Moura Jácome
-//
-// SPDX-License-Identifier: MIT
-
-#include "users.h"
-
-namespace user {}  // namespace user
